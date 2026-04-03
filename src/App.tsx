71677fb1ce15e70fafbd7517a511db786da67d3f
Mahmudul Mashrafe <mmashrafe223115@bscse.uiu.ac.bd>
2026-04-03 17:06:34 +0600

    Remove unwanted fade-in animation on section navigation

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,6 @@ import Index from "./pages/Index.tsx";
 
 const queryClient = new QueryClient();
 
-const SectionAnimationHandler = () => {
-  useEffect(() => {
-    const handleHashChange = () => {
-      // When navigating to a section via navbar, add fade-in animation
-      const id = window.location.hash.slice(1);
-      if (id) {
-        setTimeout(() => {
-          const element = document.getElementById(id);
-          if (element) {
-            element.classList.add("animate-fade-in");
-          }
-        }, 150);
-      }
-    };
-
-    window.addEventListener("hashchange", handleHashChange);
-    return () => window.removeEventListener("hashchange", handleHashChange);
-  }, []);
-
-  return null;
-};
-
 const HashScrollHandler = () => {
   useEffect(() => {
     const handleHashChange = () => {
@@ -64,7 +42,6 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <SectionAnimationHandler />
       <HashScrollHandler />
       <Index />
     </TooltipProvider>
